fea534fbbc5099988f45a2b25b74c40d7f541374
Nick Burris <nickburris13@gmail.com>
2016-08-07 20:30:13 -0300

    No commend

--- core/src/com/liamhartery/pinch/PinchGame.java
+++ core/src/com/liamhartery/pinch/PinchGame.java
@@ -4,8 +4,7 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-// this is a silly commend pushed directly to master and should probably be deleted
-// "commend"... it's comment you idiot
+// this is a silly comment pushed directly to master and should probably be deleted
 
 public class PinchGame extends Game {
 	public SpriteBatch batch;
